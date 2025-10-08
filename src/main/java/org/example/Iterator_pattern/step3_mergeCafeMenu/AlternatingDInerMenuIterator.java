154e152560f3b3b2f5f822ecd694ebd1e7b7c0aa
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2025-10-08 07:14:51 -0500

    Added WaitressCompactAkaClient and AlternatingDinerMenu in step 3

---
+++ src/main/java/org/example/Iterator_pattern/step3_mergeCafeMenu/AlternatingDInerMenuIterator.java
@@ -0,0 +1,36 @@
+package org.example.Iterator_pattern.step3_mergeCafeMenu;
+
+
+import java.util.Calendar;
+import java.util.Iterator;
+
+public class AlternatingDInerMenuIterator implements Iterator<MenuItem> {
+    MenuItem[] items;
+    int position =0;
+
+    public AlternatingDInerMenuIterator(MenuItem[] items, int position) {
+        this.items = items;
+        this.position = Calendar.DAY_OF_WEEK%2; //we wanna change menu every 2 days
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(position >= items.length || items[position] == null){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public MenuItem next() {
+        MenuItem item = items[position];
+        position++;
+        return item;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException(
+                "Alternating Diner Menu Iterator doesn't support remove");
+    }
+}
