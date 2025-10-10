ec944d22d74c09a9eedf50ce872cd3318c6ebb87
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2025-10-10 17:28:03 -0500

    added composite pattern as step 4 in iterator

---
+++ src/main/java/org/example/Iterator_pattern/step4_compositePattern/MenuItem.java
@@ -0,0 +1,46 @@
+package org.example.Iterator_pattern.step4_compositePattern;
+
+public class MenuItem extends MenuComponent{
+
+    String name;
+    String description;
+    boolean vegetarian;
+    double price;
+
+    public MenuItem(String name, String description, boolean vegetarian, double price) {
+        this.name = name;
+        this.description = description;
+        this.vegetarian = vegetarian;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    @Override
+    public void print() {
+        System.out.print("  " + getName());
+        if(isVegetarian()){
+            System.out.println("(v)");
+        }
+        System.out.println(", " + getPrice());
+        System.out.println("    -- "+ getDescription());
+    }
+}
---
+++ src/main/java/org/example/Iterator_pattern/step4_compositePattern/Waitress.java
@@ -0,0 +1,14 @@
+package org.example.Iterator_pattern.step4_compositePattern;
+
+public class Waitress {
+
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu(){
+        allMenus.print();
+    }
+}
