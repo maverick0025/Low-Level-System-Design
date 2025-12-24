4ecbc8c575d68654c9dc4493a08427c29d2eab68
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2025-12-24 15:01:01 -0600

    facade pattern

---
+++ src/main/java/org/example/facadePattern/HomeTheatreInterface.java
@@ -0,0 +1,8 @@
+package org.example.facadePattern;
+
+public interface HomeTheatreInterface {
+    void watchMovie(String movie);
+    void endMovie();
+    void listenToRadio(float frequency);
+    void endRadio();
+}
---
+++ src/main/java/org/example/facadePattern/TestHomeTheatre.java
@@ -0,0 +1,23 @@
+package org.example.facadePattern;
+
+import org.example.facadePattern.homeTheatre.*;
+
+public class TestHomeTheatre {
+    public static void main(String[] args) {
+        Amplifier amp = new Amplifier("Amplifier");
+        Tuner tuner = new Tuner("AM/FM Tuner", amp);
+        StreamingPlayer player = new StreamingPlayer("Streaming Player", amp);
+        CdPlayer cd = new CdPlayer("CD Player", amp);
+        Projector projector = new Projector("Projector", player);
+        TheaterLights lights = new TheaterLights("Theater Ceiling Lights");
+        Screen screen = new Screen("Theater Screen");
+        PopcornPopper popper = new PopcornPopper("Popcorn Popper");
+
+        HomeTheatreInterface homeTheater =
+                new HomeTheatreFacade(amp, popper, player, projector, screen, tuner, lights);
+
+        homeTheater.watchMovie("Raiders of the Lost Ark");
+        System.out.println("movie done!");
+        homeTheater.endMovie();
+    }
+}
