313d231cddf81a14ee85c9bdb5980c05573e10c2
Satya Ashok Dowluri <dowlurisatyaashok@gmail.com>
2026-01-19 22:23:42 -0600

    add sliding window log rate limiter

--- src/main/java/org/example/llds/rateLimiter/limiter/SlidingWindowLogRateLimiter.java
+++ src/main/java/org/example/llds/rateLimiter/limiter/SlidingWindowLogRateLimiter.java
@@ -3,15 +3,41 @@ package org.example.llds.rateLimiter.limiter;
 import org.example.llds.rateLimiter.enums.RateLimitType;
 import org.example.llds.rateLimiter.model.RateLimitConfig;
 
+import java.util.ArrayDeque;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class SlidingWindowLogRateLimiter extends RateLimiter{
 
     public SlidingWindowLogRateLimiter(RateLimitConfig config) {
         super(RateLimitType.SLIDING_WINDOW_LOG, config);
     }
+    private final Map<String, Queue<Long>> requestLog = new ConcurrentHashMap<>();
+    //user id-> request queue only successful requests until now
 
     @Override
     public boolean allowRequest(String userId) {
-        System.out.println("Yet to implement");
-        return false;
+
+        AtomicBoolean allowed = new AtomicBoolean();
+        long now = System.currentTimeMillis()/1000; //current time in seconds
+
+        requestLog.compute(userId, (id, log)->{
+           if(log == null) log = new ArrayDeque<>();
+
+           while(!log.isEmpty() &&
+                   (log.peek()-now >= config.getWindowInSeconds())){
+               log.poll();
+           }
+
+           if(log.size() < config.getMaxRequests()){
+               log.add(now);
+               allowed.set(true);
+           }
+            return log;
+        });
+
+        return allowed.get();
     }
 }
